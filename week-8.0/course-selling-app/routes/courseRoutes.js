3c617d010533fd1cb9f63e7557f1b634a737f64a
Ashish Singh <psssasds212004@gmail.com>
2025-08-08 18:25:53 +0000

    add skeleton for userRoutes

---
+++ week-8.0/course-selling-app/routes/courseRoutes.js
@@ -0,0 +1,18 @@
+const {Router} = require("express");
+const courseRouter = Router();
+
+courseRouter.post("/signup", (req, res) => {
+
+})
+
+courseRouter.post("/signin", (req, res) => {
+
+})
+
+courseRouter.post("/purchases", (req, res) => {
+
+})
+
+module.exports({
+  courseRouter
+})
