43aa13995f58951eba1b3b0ea8d5ff8a53d9d34d
Ashish Singh <psssasds212004@gmail.com>
2025-08-09 20:42:10 +0000

    add better code for server

--- week-8.0/course-selling-app/server.js
+++ week-8.0/course-selling-app/server.js
@@ -1,11 +1,34 @@
 const express = require("express");
-const {adminRouter} = require("./routes/adminRoutes");
-const {userRouter} = require("./routes/userRoutes");
-const {courseRouter} = require("./routes/courseRoutes");
+require('dotenv').config();
+
+const adminRouter = require("./routes/adminRoutes");
+const userRouter = require("./routes/userRoutes");
+const courseRouter = require("./routes/courseRoutes");
+
+const mongoose = require("mongoose");
+
 const app = express();
+const PORT = 3000;
+
+app.use(express.json());
 
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
 
-app.listen(3000);
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("✅ Successfully connected to MongoDB!");
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server is running on http://localhost:${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("❌ Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
