ab3ad475880edee4959a92b92efd7baf2a8a27e7
Ashish Singh <psssasds212004@gmail.com>
2025-08-10 21:24:31 +0000

    course routes completed

--- week-8.0/course-selling-app/backend/routes/courseRoutes.js
+++ week-8.0/course-selling-app/backend/routes/courseRoutes.js
@@ -8,15 +8,31 @@ const {courseModel} = require("../models/course")
 const {purchaseModel} = require("../models/purchase");
 const {userModel} = require("../models/user");
 
-// If User want to purchase any course
-courseRouter.post("/purchase", (req, res) => {
+const {userMiddleware} = require("../middleware/userAuth");
 
+// If User want to purchase any course
+courseRouter.post("/purchase", userMiddleware, async (req, res) => {
+  const userId = req.userId;
+  const courseId = req.body.courseId;
+
+  await purchaseModel.create({
+    userId,
+    courseId
+  })
+
+  res.json({
+    message: "Course purchased Successfully!!"
+  })
 })
 
 // All Courses preview
-courseRouter.get("/preview", (req, res) => {
+courseRouter.get("/preview", async (req, res) => {
+  // Finding all the courses from course model
+  const courses = await courseModel.find({});
+
+  // Displaying all the courses
   res.json({
-    message: "These are the Courses available"
+    courses
   })
 })
 
