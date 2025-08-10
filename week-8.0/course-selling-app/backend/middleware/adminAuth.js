1f37fbd0193947469eb65867aa45b782969c82b9
Ashish Singh <psssasds212004@gmail.com>
2025-08-10 14:35:39 +0000

    add comments

--- week-8.0/course-selling-app/backend/middleware/adminAuth.js
+++ week-8.0/course-selling-app/backend/middleware/adminAuth.js
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const adminMiddleware = (req, res, next) => {
+  // Input Token from headers
   const token = req.headers.authorization;
 
+  // Input Token from headers
   if(!token){
     res.status(401).json({
       message: "No token, Access Denied!!"
     })
   }
 
+  // Verifying token is correct or not and passing control to next
   try{
     const decodedInfo = jwt.verify(token, process.env.JWT_ADMIN_SECRET);
     req.adminId = decodedInfo.adminId;
--- week-8.0/course-selling-app/backend/middleware/userAuth.js
+++ week-8.0/course-selling-app/backend/middleware/userAuth.js
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const userMiddleware = (req, res, next) => {
+  // Input Token from headers
   const token = req.headers.authorization;
 
+  // Checking if token is given or not
   if(!token){
     res.status(401).json({
       message: "No token, Access Denied!!"
     })
   }
 
+  // Verifying token is correct or not and passing control to next
   try{
     const decodedInfo = jwt.verify(token, process.env.JWT_USER_SECRET);
     req.userId = decodedInfo.userId;
