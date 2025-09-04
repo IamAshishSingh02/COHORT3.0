9afb42d1a22239c98fe0f34f59014f498c401f08
Ashish Singh <psssasds212004@gmail.com>
2025-09-04 13:30:53 +0000

    done with the post part

--- week-9.0/react-basics2/src/App.jsx
+++ week-9.0/react-basics2/src/App.jsx
@@ -1,74 +1,34 @@
+import { useState } from "react";
+import { PostComponent } from "./Post";
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  function addPost() {
+    setPosts([...posts, {
+      name: "Ashish Singh",
+      subtitle: "500 followers",
+      time: "2m ago",
+      image: "/profile-pic1.jpeg",
+      description: "What to know how to win big? Check out how these folks won $6000 in bounties."
+    }])
+  }
+
+  const postComponent = posts.map(post => <PostComponent 
+    name = {post.name}
+    subtitle = {post.subtitle}
+    time = {post.time}
+    image = {post.image}
+    description = {post.description}
+  />)
+
   return (
-    <div>
-      <div>
-        <PostComponent 
-          name = {"Ashish Singh"}
-          subtitle = {"500 followers"}
-          time = {"2m ago"}
-          image = {"/profile-pic1.jpeg"}
-          description = {"What to know how to win big? Check out how these folks won $6000 in bounties."}
-        />;
+    <div style={{ background: "#dfe6e9", minHeight: "100vh" }}>
+      <button onClick={addPost}>Add Post</button>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem" }}>
+        {postComponent}
       </div>
-
-      <div>
-        <PostComponent 
-          name = {"Harkirat"}
-          subtitle = {"Promoted"}
-          image = {"/profile-pic2.jpg"}
-          description = {"How to get hired in 2024? I lost my Job in 2023, this is the roadmap I followed to get hired in 2024."}
-        />;
-      </div>
-
-      {/* <div>
-        <PostComponent />;
-      </div> */}
     </div>
-  ) 
-}
-
-function PostComponent({name, subtitle, time, image, description}) {
-  const style = {
-    backgroundColor: "#dfe6e9",
-    width: 400,
-    borderRadius: 10,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderStyle: "solid",
-    padding: 10
-  };
-
-  return (
-    <>
-      <div style={style}>
-        <div style={{display: "flex"}}>
-          <div>
-            <img
-              src={image}
-              style={{ width: 50, height: 50, borderRadius: 50 }}
-              alt="Profile"
-            />
-          </div>
-
-          <div style={{padding: 5, fontSize: 14}}>
-            <div><b>{name}</b></div>
-            <div>{subtitle}</div>
-            {time && <div style={{display: "flex"}}>
-              <div>{time}</div>
-              <div>
-                <img 
-                  src="/clock.webp" 
-                  style={{width: 12, height: 12}}
-                  alt="clock" 
-                />
-              </div>
-            </div>}
-          </div>
-        </div>
-
-        <div style={{ fontSize: 14 }}>{description}</div>
-      </div>
-    </>
   )
 }
 
