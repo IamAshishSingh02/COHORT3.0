a5e15f05e1807f6b310e30a46ab96066d28c51de
Ashish Singh <psssasds212004@gmail.com>
2025-09-05 09:15:12 +0000

    add bell icon for notification bar

--- week-9.0/react-basics2/src/App.jsx
+++ week-9.0/react-basics2/src/App.jsx
@@ -2,32 +2,38 @@ import { useState } from "react";
 import { PostComponent } from "./Post";
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  // const [posts, setPosts] = useState([]);
 
-  function addPost() {
-    setPosts([...posts, {
-      name: "Ashish Singh",
-      subtitle: "500 followers",
-      time: "2m ago",
-      image: "/profile-pic1.jpeg",
-      description: "What to know how to win big? Check out how these folks won $6000 in bounties."
-    }])
-  }
+  // function addPost() {
+  //   setPosts([...posts, {
+  //     name: "Ashish Singh",
+  //     subtitle: "500 followers",
+  //     time: "2m ago",
+  //     image: "/profile-pic1.jpeg",
+  //     description: "What to know how to win big? Check out how these folks won $6000 in bounties."
+  //   }])
+  // }
 
-  const postComponent = posts.map(post => <PostComponent 
-    name = {post.name}
-    subtitle = {post.subtitle}
-    time = {post.time}
-    image = {post.image}
-    description = {post.description}
-  />)
+  // const postComponent = posts.map(post => <PostComponent 
+  //   name = {post.name}
+  //   subtitle = {post.subtitle}
+  //   time = {post.time}
+  //   image = {post.image}
+  //   description = {post.description}
+  // />)
+
+  // return (
+  //   <div style={{ background: "#dfe6e9", minHeight: "100vh" }}>
+  //     <button onClick={addPost}>Add Post</button>
+  //     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem" }}>
+  //       {postComponent}
+  //     </div>
+  //   </div>
+  // )
 
   return (
-    <div style={{ background: "#dfe6e9", minHeight: "100vh" }}>
-      <button onClick={addPost}>Add Post</button>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem" }}>
-        {postComponent}
-      </div>
+    <div>
+      <img src="/bell-icon.webp" alt="bell-icon" />
     </div>
   )
 }
