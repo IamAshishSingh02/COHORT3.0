dce9768571e2871f936a5ea2975bc087a3fd7310
Ashish Singh <psssasds212004@gmail.com>
2025-09-11 13:13:30 +0000

    learned about children props

--- week-9.0/react-basics3/src/App.jsx
+++ week-9.0/react-basics3/src/App.jsx
@@ -1,5 +1,29 @@
-function App() {
+const App = () => {
+  return(
+    <div>
+      <Card>
+        <div> 
+           <h2>Card Title</h2>
+           <p>This is some content inside card.</p>
+        </div>
+      </Card>
 
+      <Card>
+        <div>
+          <h2>Another Card</h2>
+          <p>Card with different content.</p>
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+const Card = ({children}) => {
+  return(
+    <div style={{border: "1px solid #ccc",borderRadius: 4, padding: "20px", margin: "10px", boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)" }}>
+      {children}
+    </div>
+  )
 }
 
 export default App
