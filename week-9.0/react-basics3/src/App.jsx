1139c3c564c0ff168a6633ffa897bf3d3fc7f653
Ashish Singh <psssasds212004@gmail.com>
2025-09-12 11:34:28 +0000

    learned about lists and keys

--- week-9.0/react-basics3/src/App.jsx
+++ week-9.0/react-basics3/src/App.jsx
@@ -1,27 +1,57 @@
+// Children Props
+// const App = () => {
+//   return(
+//     <div>
+//       <Card>
+//         <div> 
+//            <h2>Card Title</h2>
+//            <p>This is some content inside card.</p>
+//         </div>
+//       </Card>
+
+//       <Card>
+//         <div>
+//           <h2>Another Card</h2>
+//           <p>Card with different content.</p>
+//         </div>
+//       </Card>
+//     </div>
+//   )
+// }
+
+// const Card = ({children}) => {
+//   return(
+//     <div style={{border: "1px solid #ccc",borderRadius: 4, padding: "20px", margin: "10px", boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)" }}>
+//       {children}
+//     </div>
+//   )
+// }
+
+// Lists And Keys
 const App = () => {
+  const todos = [{
+    key: 1, 
+    title: "Go to gym",
+    done: false
+  }, {
+    key: 2,
+    title: "Eat food",
+    done: true
+  }]
+
+  const todosComponent = todos.map(todo => <Todo key = {todo.key} title = {todo.title} done = {todo.done} />)
+
   return(
     <div>
-      <Card>
-        <div> 
-           <h2>Card Title</h2>
-           <p>This is some content inside card.</p>
-        </div>
-      </Card>
-
-      <Card>
-        <div>
-          <h2>Another Card</h2>
-          <p>Card with different content.</p>
-        </div>
-      </Card>
+      {todosComponent}
     </div>
   )
 }
 
-const Card = ({children}) => {
+const Todo = ({title, done}) => {
   return(
-    <div style={{border: "1px solid #ccc",borderRadius: 4, padding: "20px", margin: "10px", boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)" }}>
-      {children}
+    <div>
+      {title} - {done ? "Done" : "Not Done"}
     </div>
   )
 }
