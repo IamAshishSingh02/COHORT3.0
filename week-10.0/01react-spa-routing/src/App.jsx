38ef1c1cfeabc20840651ef9a0c6053a87da91f1
Ashish Singh <psssasds212004@gmail.com>
2025-09-14 18:15:44 +0000

    learned about focusing on an Input box using useRef hook

--- week-10.0/01react-spa-routing/src/App.jsx
+++ week-10.0/01react-spa-routing/src/App.jsx
@@ -1,100 +1,124 @@
-import {BrowserRouter, Routes, Route, Link, useNavigate, Outlet} from "react-router-dom";
+// import {BrowserRouter, Routes, Route, Link, useNavigate, Outlet} from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-            <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
-}
+import { useRef } from "react"
 
-const Layout = () => {
-  return(
-    <div style={{height: "100vh"}}>
-      <Header />
+// function App() {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route path="/" element={<LandingPage />} />
+//             <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+//             <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+//             <Route path="*" element={<ErrorPage />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   )
+// }
 
-      <div style={{height: "75vh"}}>
-        <Outlet />
-      </div>
+// const Layout = () => {
+//   return(
+//     <div style={{height: "100vh"}}>
+//       <Header />
 
-      <Footer />
-    </div>
-  )
-}
+//       <div style={{height: "75vh"}}>
+//         <Outlet />
+//       </div>
 
-const Header = () => {
-  return(
-    <div>
-      <Link to={"/"}>
-        <button style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Allen</button>
-      </Link>
+//       <Footer />
+//     </div>
+//   )
+// }
 
-      <Link to={"/neet/online-coaching-class-11"}>
-        <button style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Class 11</button>
-      </Link>
-      
-      <Link to={"/neet/online-coaching-class-12"}>
-        <button style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Class 12</button>
-      </Link>
-    </div>
-  )
-}
+// const Header = () => {
+//   return(
+//     <div>
+//       <Link to={"/"}>
+//         <button style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Allen</button>
+//       </Link>
 
-const Footer = () => {
-  return(
-    <div style={{color: "white"}}>
-      ------------------------
-      <br />
-      Footer | Contact Us
-      <br />
-      ------------------------
-    </div>
-  )
-}
+//       <Link to={"/neet/online-coaching-class-11"}>
+//         <button style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Class 11</button>
+//       </Link>
 
-const ErrorPage = () => {
-  return(
-    <div style={{color: "white"}}>
-      <h2>Content not Found</h2>
-    </div>
-  )
-}
+//       <Link to={"/neet/online-coaching-class-12"}>
+//         <button style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Class 12</button>
+//       </Link>
+//     </div>
+//   )
+// }
 
-const LandingPage = () => {
-  return(
-    <div style={{color: "white"}}>
-      <h2>Welcome to Allen</h2>
-    </div>
-  )
-}
-const Class11Program = () => {
-  return(
-    <div style={{color: "white"}}>
-      <h2>NEET program for Class 11th</h2>
-    </div>
-  )
-}
+// const Footer = () => {
+//   return(
+//     <div style={{color: "white"}}>
+//       ------------------------
+//       <br />
+//       Footer | Contact Us
+//       <br />
+//       ------------------------
+//     </div>
+//   )
+// }
 
-const Class12Program = () => {
-  const navigate = useNavigate();
+// const ErrorPage = () => {
+//   return(
+//     <div style={{color: "white"}}>
+//       <h2>Content not Found</h2>
+//     </div>
+//   )
+// }
 
-  const redirectUser = () => {
-    navigate("/")
-  }
+// const LandingPage = () => {
+//   return(
+//     <div style={{color: "white"}}>
+//       <h2>Welcome to Allen</h2>
+//     </div>
+//   )
+// }
+// const Class11Program = () => {
+//   return(
+//     <div style={{color: "white"}}>
+//       <h2>NEET program for Class 11th</h2>
+//     </div>
+//   )
+// }
+
+// const Class12Program = () => {
+//   const navigate = useNavigate();
+
+//   const redirectUser = () => {
+//     navigate("/")
+//   }
   
+//   return(
+//     <div style={{color: "white"}}>
+//       <h2>NEET program for Class 12th</h2>
+//       <button onClick={redirectUser} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Go back to Landing Page</button>
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const inputRef = useRef() 
+
+  const focusOnInput = () => {
+    inputRef.current.focus()
+  }
   return(
-    <div style={{color: "white"}}>
-      <h2>NEET program for Class 12th</h2>
-      <button onClick={redirectUser} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Go back to Landing Page</button>
+    <div style={{color: "white", margin: "20px"}}>
+      Sign Up
+      <br />
+      <br />
+      <input ref={inputRef} type="text" placeholder="Username" />
+      <br />
+      <br />
+      <input type="text" placeholder="Password" />
+      <br />
+      <br />
+      <button onClick={focusOnInput} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Submit</button>
     </div>
   )
 }
