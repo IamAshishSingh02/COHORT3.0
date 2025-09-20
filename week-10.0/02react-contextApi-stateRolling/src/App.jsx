02d76c8663ec0f76a57a2944bc6e2c186d9d3a77
Ashish Singh <psssasds212004@gmail.com>
2025-09-20 12:34:35 +0000

    learned about Prop Drilling and problems related to that

--- week-10.0/02react-contextApi-stateRolling/src/App.jsx
+++ week-10.0/02react-contextApi-stateRolling/src/App.jsx
@@ -1,28 +1,80 @@
 import { useState } from "react"
 
+// Rolling up the State and Unoptimal Rerenders
+// const App = () => {
+//   return(
+//     <>
+//       <div>
+//         <Light />
+//       </div>
+//     </>
+//   )
+// }
+
+// const Light = () => {
+//   const [bulbOn, setBulbOn] = useState(true)
+//   return(
+//     <>
+//       <div>
+//         <LightBulb bulbOn = {bulbOn} />
+//         <LightSwitch setBulbOn = {setBulbOn} />
+//       </div>
+//     </>
+//   )
+// }
+
+// const LightBulb = ({bulbOn}) => {
+//   const img1 = <img style={{width: "30px"}} src="/Light_on.webp" /> 
+//   const img2 = <img style={{width: "30px"}} src="/Light_off.webp" />
+  
+//   return(
+//     <>
+//       <div style={{margin: "10px"}}>
+//         {bulbOn ? img1 : img2 }
+//       </div>
+//     </>
+//   )
+// }
+
+// const LightSwitch = ({setBulbOn}) => {
+//   const toggle = () => {
+//     setBulbOn(s => !s)
+//   }
+
+//   return(
+//     <>
+//       <div>
+//         <button onClick={toggle} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Toggle the bulb</button>
+//       </div>
+//     </>
+//   )
+// }
+
+// Prop Drilling
 const App = () => {
-  return(
-    <>
-      <div>
-        <LightBulb />
-      </div>
-    </>
-  )
-}
-
-const LightBulb = () => {
   const [bulbOn, setBulbOn] = useState(true)
+
   return(
     <>
       <div>
-        <BulbState bulbOn = {bulbOn} />
-        <ToggleBulbState setBulbOn = {setBulbOn} />
+        <Light bulbOn = {bulbOn} setBulbOn = {setBulbOn} />
       </div>
     </>
   )
 }
 
-const BulbState = ({bulbOn}) => {
+const Light = ({bulbOn, setBulbOn}) => {
+  return(
+    <>
+      <div>
+        <LightBulb bulbOn = {bulbOn} />
+        <LightSwitch setBulbOn = {setBulbOn} />
+      </div>
+    </>
+  )
+}
+
+const LightBulb = ({bulbOn}) => {
   const img1 = <img style={{width: "30px"}} src="/lightbulb_on.webp" /> 
   const img2 = <img style={{width: "30px"}} src="/lightbulb_off.webp" />
   
@@ -35,7 +87,7 @@ const BulbState = ({bulbOn}) => {
   )
 }
 
-const ToggleBulbState = ({setBulbOn}) => {
+const LightSwitch = ({setBulbOn}) => {
   const toggle = () => {
     setBulbOn(s => !s)
   }
@@ -43,7 +95,7 @@ const ToggleBulbState = ({setBulbOn}) => {
   return(
     <>
       <div>
-        <button onClick={toggle} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Toogle the bulb</button>
+        <button onClick={toggle} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Toggle the bulb</button>
       </div>
     </>
   )
