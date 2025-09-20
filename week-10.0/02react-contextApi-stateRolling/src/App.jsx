9ab73200f43669ef8c8c7ced485405158f67f872
Ashish Singh <psssasds212004@gmail.com>
2025-09-20 11:52:09 +0000

    Learned about Rolling up the State and Unoptimal Rerenders by implementing a simple toogle bulb app.

--- week-10.0/02react-contextApi-stateRolling/src/App.jsx
+++ week-10.0/02react-contextApi-stateRolling/src/App.jsx
@@ -1,5 +1,52 @@
-function App() {
+import { useState } from "react"
+
+const App = () => {
+  return(
+    <>
+      <div>
+        <LightBulb />
+      </div>
+    </>
+  )
+}
+
+const LightBulb = () => {
+  const [bulbOn, setBulbOn] = useState(true)
+  return(
+    <>
+      <div>
+        <BulbState bulbOn = {bulbOn} />
+        <ToggleBulbState setBulbOn = {setBulbOn} />
+      </div>
+    </>
+  )
+}
+
+const BulbState = ({bulbOn}) => {
+  const img1 = <img style={{width: "30px"}} src="/lightbulb_on.webp" /> 
+  const img2 = <img style={{width: "30px"}} src="/lightbulb_off.webp" />
   
+  return(
+    <>
+      <div style={{margin: "10px"}}>
+        {bulbOn ? img1 : img2 }
+      </div>
+    </>
+  )
+}
+
+const ToggleBulbState = ({setBulbOn}) => {
+  const toggle = () => {
+    setBulbOn(s => !s)
+  }
+
+  return(
+    <>
+      <div>
+        <button onClick={toggle} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Toogle the bulb</button>
+      </div>
+    </>
+  )
 }
 
 export default App
