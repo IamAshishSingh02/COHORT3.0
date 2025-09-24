50347be919c0899d1cd44db6e27b3929f93212f4
Ashish Singh <psssasds212004@gmail.com>
2025-09-24 14:07:27 +0000

    learned about context Api, created a bulb provider

--- week-10.0/02react-contextApi-stateRolling/src/App.jsx
+++ week-10.0/02react-contextApi-stateRolling/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { createContext, useContext, useState } from "react"
 
 // Rolling up the State and Unoptimal Rerenders
 // const App = () => {
@@ -51,32 +51,97 @@ import { useState } from "react"
 // }
 
 // Prop Drilling
-const App = () => {
+// const App = () => {
+//   const [bulbOn, setBulbOn] = useState(true)
+
+//   return(
+//     <>
+//       <div>
+//         <Light bulbOn = {bulbOn} setBulbOn = {setBulbOn} />
+//       </div>
+//     </>
+//   )
+// }
+
+// const Light = ({bulbOn, setBulbOn}) => {
+//   return(
+//     <>
+//       <div>
+//         <LightBulb bulbOn = {bulbOn} />
+//         <LightSwitch setBulbOn = {setBulbOn} />
+//       </div>
+//     </>
+//   )
+// }
+
+// const LightBulb = ({bulbOn}) => {
+//   const img1 = <img style={{width: "30px"}} src="/lightbulb_on.webp" /> 
+//   const img2 = <img style={{width: "30px"}} src="/lightbulb_off.webp" />
+  
+//   return(
+//     <>
+//       <div style={{margin: "10px"}}>
+//         {bulbOn ? img1 : img2 }
+//       </div>
+//     </>
+//   )
+// }
+
+// const LightSwitch = ({setBulbOn}) => {
+//   const toggle = () => {
+//     setBulbOn(s => !s)
+//   }
+
+//   return(
+//     <>
+//       <div>
+//         <button onClick={toggle} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Toggle the bulb</button>
+//       </div>
+//     </>
+//   )
+// }
+
+// Context Api
+const BulbContext = createContext();
+
+const BulbProvider = ({children}) => {
   const [bulbOn, setBulbOn] = useState(true)
 
   return(
-    <>
-      <div>
-        <Light bulbOn = {bulbOn} setBulbOn = {setBulbOn} />
-      </div>
-    </>
+    <BulbContext.Provider value={{bulbOn, setBulbOn}}>
+      {children}
+    </BulbContext.Provider> 
   )
 }
 
-const Light = ({bulbOn, setBulbOn}) => {
+const App = () => {
   return(
     <>
       <div>
-        <LightBulb bulbOn = {bulbOn} />
-        <LightSwitch setBulbOn = {setBulbOn} />
+        <BulbProvider>
+          <Light />
+        </BulbProvider>
       </div>
     </>
   )
 }
 
-const LightBulb = ({bulbOn}) => {
+const Light = () => {
+  return(
+    <>
+      <div>
+        <LightBulb />
+        <LightSwitch />
+      </div>
+    </>
+  )
+}
+
+const LightBulb = () => {
   const img1 = <img style={{width: "30px"}} src="/lightbulb_on.webp" /> 
   const img2 = <img style={{width: "30px"}} src="/lightbulb_off.webp" />
+
+  const {bulbOn} = useContext(BulbContext)
   
   return(
     <>
@@ -87,7 +152,9 @@ const LightBulb = ({bulbOn}) => {
   )
 }
 
-const LightSwitch = ({setBulbOn}) => {
+const LightSwitch = () => {
+  const {setBulbOn} = useContext(BulbContext)
+
   const toggle = () => {
     setBulbOn(s => !s)
   }
