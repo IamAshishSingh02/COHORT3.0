10fe4277bfa771abf59a19f49091875a3c502e8b
Ashish Singh <psssasds212004@gmail.com>
2025-09-25 18:21:46 +0000

    created a small example of custom hook

--- week-11.0/customHooks/src/App.jsx
+++ week-11.0/customHooks/src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
- 
+import { useState } from "react"
+
+// Custom Hook
+const useCounter = () => {
+  const [count, setCount] = useState(0)
+
+  const increaseCount = () => {
+    setCount(count => count + 1)
+  }
+
+  return {increaseCount, count}
+}
+
+const App = () => {
+ const {increaseCount, count} = useCounter()
+
+ return(
+  <div>
+    <button onClick={increaseCount} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Increase {count}</button>
+  </div>
+ )
 }
 
 export default App
