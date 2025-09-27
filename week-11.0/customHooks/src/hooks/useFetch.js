f80b1593ab6b9fb7782e7ce31aa865a3e7dbd8b1
Ashish Singh <psssasds212004@gmail.com>
2025-09-27 14:03:15 +0000

    added dependency array content and set loading while hitting backend

--- week-11.0/customHooks/src/hooks/useFetch.js
+++ week-11.0/customHooks/src/hooks/useFetch.js
@@ -2,16 +2,19 @@ import { useEffect, useState } from "react"
 
 export const useFetch = (url) => {
   const [post, setPost] = useState({})
+  const [loading, setLoading] = useState(true)
 
   const getPosts = async () => {
+    setLoading(true)
     const response = await fetch(url)
     const jsonData = await response.json()
     setPost(jsonData)
+    setLoading(false)
   }
 
   useEffect(() => {
     getPosts()
-  }, [])
+  }, [url])
 
-  return post
+  return {post, loading}
 }
