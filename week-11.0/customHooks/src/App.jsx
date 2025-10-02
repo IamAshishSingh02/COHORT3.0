faa9895b50761a20a4d6cf06dc3221ad6eea0a9e
Ashish Singh <psssasds212004@gmail.com>
2025-10-02 13:11:11 +0000

    implementataion of useDebounce hook

--- week-11.0/customHooks/src/App.jsx
+++ week-11.0/customHooks/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useFetch } from "./hooks/useFetch"
 import { usePrev } from "./hooks/usePrev"
+import { useDebounce } from "./hooks/useDebounce"
 
 // Custom Hook
 // const useCounter = () => {
@@ -51,17 +52,34 @@ import { usePrev } from "./hooks/usePrev"
 //   )
 // }
 
+// usePrev Custom Hook 
+// const App = () => {
+//   const [count, setCount] = useState(0)
+//   const prev = usePrev(count)
+
+//   return(
+//     <>
+//       <div style={{marginLeft: "35px"}}>{count}</div>
+
+//       <button onClick={() => setCount(c => c + 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Click Me</button>
+
+//       <div style={{margin: "10px"}}>The previous value was <b>{prev}</b>.</div>
+//     </>
+//   )
+// }
+
+
+// useDebounce Custom Hook
 const App = () => {
-  const [count, setCount] = useState(0)
-  const prev = usePrev(count)
+  const sendDataToBackend = () => {
+    fetch("api.amazon.com/search/")
+  }
+
+  const debouncedFn = useDebounce(sendDataToBackend)
 
   return(
     <>
-      <div style={{marginLeft: "35px"}}>{count}</div>
-
-      <button onClick={() => setCount(c => c + 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Click Me</button>
-
-      <div style={{margin: "10px"}}>The previous value was <b>{prev}</b>.</div>
+      <input type="text" onChange={debouncedFn} />
     </>
   )
 }
