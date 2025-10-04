8dab9f53a6d806e80a9315899cb313a220a01b65
Ashish Singh <psssasds212004@gmail.com>
2025-10-04 22:16:12 +0000

    changed counter implementation according to the recoil use case

--- week-11.0/recoil/src/App.jsx
+++ week-11.0/recoil/src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { counterAtom } from "./store/atoms/counter";
 
 function App() {
   return(
-    <>
+    < RecoilRoot>
       <Counter /> 
-    </>
+    </RecoilRoot>
   )
 }
 
 const Counter = () => {
-  const [count, setCount] = useState(0)
-
   return(
     <>
       <div>
-        <Count count = {count} />
-        <Increase setCount = {setCount} />
-        <Decrease setCount = {setCount} />  
+        <Count />
+        <Increase />
+        <Decrease />  
       </div>
     </>
   )
 }
 
-const Count = ({count}) => {
+const Count = () => {
+  const count = useRecoilValue(counterAtom)
+
   return(
     <div style={{marginLeft: "85px"}}>
       {count}
@@ -30,13 +31,17 @@ const Count = ({count}) => {
   )
 }
 
-const Increase = ({setCount}) => {
+const Increase = () => {
+  const setCount = useSetRecoilState(counterAtom)
+
   return(
     <button onClick={() => setCount(c => c + 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Increase</button>
   )
 }
 
-const Decrease = ({setCount}) => {
+const Decrease = () => {
+  const setCount = useSetRecoilState(counterAtom)
+
   return(
     <button onClick={() => setCount(c => c - 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Decrease</button>
   )
