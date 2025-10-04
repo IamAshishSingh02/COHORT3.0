947e105467606610e3f5073bb4aad61843e1a954
Ashish Singh <psssasds212004@gmail.com>
2025-10-04 21:31:04 +0000

    build a basic counter

--- week-11.0/recoil/src/App.jsx
+++ week-11.0/recoil/src/App.jsx
@@ -1,5 +1,45 @@
-function App() {
+import { useState } from "react";
 
+function App() {
+  return(
+    <>
+      <Counter /> 
+    </>
+  )
+}
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  return(
+    <>
+      <div>
+        <Count count = {count} />
+        <Increase setCount = {setCount} />
+        <Decrease setCount = {setCount} />  
+      </div>
+    </>
+  )
+}
+
+const Count = ({count}) => {
+  return(
+    <div style={{marginLeft: "85px"}}>
+      {count}
+    </div>
+  )
+}
+
+const Increase = ({setCount}) => {
+  return(
+    <button onClick={() => setCount(c => c + 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Increase</button>
+  )
+}
+
+const Decrease = ({setCount}) => {
+  return(
+    <button onClick={() => setCount(c => c - 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Decrease</button>
+  )
 }
 
 export default App
