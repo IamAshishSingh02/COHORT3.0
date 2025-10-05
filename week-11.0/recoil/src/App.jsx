e08a5b492f94841776b4f5a176bdb0842d08fb28
Ashish Singh <psssasds212004@gmail.com>
2025-10-05 19:12:04 +0000

    learned about memo implementation

--- week-11.0/recoil/src/App.jsx
+++ week-11.0/recoil/src/App.jsx
@@ -1,50 +1,104 @@
-import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
-import { counterAtom } from "./store/atoms/counter";
+// Recoil
 
-function App() {
-  return(
-    < RecoilRoot>
-      <Counter /> 
-    </RecoilRoot>
-  )
+// import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+// import { counterAtom } from "./store/atoms/counter";
+
+// function App() {
+//   return(
+//     < RecoilRoot>
+//       <Counter /> 
+//     </RecoilRoot>
+//   )
+// }
+
+// const Counter = () => {
+//   return(
+//     <>
+//       <div>
+//         <Count />
+//         <Increase />
+//         <Decrease />  
+//       </div>
+//     </>
+//   )
+// }
+
+// const Count = () => {
+//   const count = useRecoilValue(counterAtom)
+
+//   return(
+//     <div style={{marginLeft: "85px"}}>
+//       {count}
+//     </div>
+//   )
+// }
+
+// const Increase = () => {
+//   const setCount = useSetRecoilState(counterAtom)
+
+//   return(
+//     <button onClick={() => setCount(c => c + 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Increase</button>
+//   )
+// }
+
+// const Decrease = () => {
+//   const setCount = useSetRecoilState(counterAtom)
+
+//   return(
+//     <button onClick={() => setCount(c => c - 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Decrease</button>
+//   )
+// }
+
+// Memo Implementation
+
+import { useState, useEffect, memo } from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 const Counter = () => {
-  return(
-    <>
-      <div>
-        <Count />
-        <Increase />
-        <Decrease />  
-      </div>
-    </>
-  )
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(c => c + 1); 
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [])
+
+  return (
+    <div>
+      <Count count={count} />
+      <Increase setCount={setCount} />
+      <Decrease setCount={setCount} />
+    </div>
+  );
 }
 
-const Count = () => {
-  const count = useRecoilValue(counterAtom)
-
-  return(
-    <div style={{marginLeft: "85px"}}>
+const Count = memo(function({ count }) {
+  return (
+    <div>
       {count}
     </div>
   )
-}
+})
 
-const Increase = () => {
-  const setCount = useSetRecoilState(counterAtom)
+const Decrease = memo(function({ setCount }) {
+  return (
+    <button onClick={() => setCount(c => c - 1)}>Decrease</button>
+  ); 
+});
 
-  return(
-    <button onClick={() => setCount(c => c + 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Increase</button>
+const Increase = memo(function({ setCount }) {
+  return (
+    <button onClick={() => setCount(c => c + 1)}>Increase</button>
   )
-}
-
-const Decrease = () => {
-  const setCount = useSetRecoilState(counterAtom)
-
-  return(
-    <button onClick={() => setCount(c => c - 1)} style={{padding: "8px", margin: "10px", border: "2px solid", borderRadius: "4px", cursor: "pointer", boxShadow: "2px 5px rgba(0, 0, 0, 0.2)"}}>Decrease</button>
-  )
-}
+})
 
 export default App
