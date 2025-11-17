a1cffda019e04a33018e5ab145de76267f22ee80
Ashish Singh <psssasds212004@gmail.com>
2025-11-17 17:58:27 +0000

    learned about responsiveness in tailwind and commenting preivous work

--- week-13.0/tailwind-part1/src/App.jsx
+++ week-13.0/tailwind-part1/src/App.jsx
@@ -12,16 +12,20 @@ const App = () => {
     //   </div>
     // </div>
 
-    <div className="grid grid-cols-12">
-      <div className="bg-red-300 col-span-4">
-        Child 1
-      </div>
-      <div className="bg-blue-300 col-span-5">
-        Child 2
-      </div>
-      <div className="bg-green-300 col-span-3">
-        Child 3
-      </div>
+    // <div className="grid grid-cols-12">
+    //   <div className="bg-red-300 col-span-4">
+    //     Child 1
+    //   </div>
+    //   <div className="bg-blue-300 col-span-5">
+    //     Child 2
+    //   </div>
+    //   <div className="bg-green-300 col-span-3">
+    //     Child 3
+    //   </div>
+    // </div>
+
+    <div className="xl:bg-yellow-300 md:bg-green-300 sm:bg-blue-300 bg-red-300">
+      Hi there
     </div>
   )
 }
