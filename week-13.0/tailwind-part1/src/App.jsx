8f876ed33965b9663af9b32c7202cfff7c629e1f
Ashish Singh <psssasds212004@gmail.com>
2025-11-17 17:26:16 +0000

    learned about flex in tailwind

--- week-13.0/tailwind-part1/src/App.jsx
+++ week-13.0/tailwind-part1/src/App.jsx
@@ -1,1 +1,17 @@
+const App = () => {
+  return(
+    <div className="flex justify-around ">
+      <div className="bg-red-300">
+        Child 1
+      </div>
+      <div className="bg-blue-300">
+        Child 2
+      </div>
+      <div className="bg-green-300">
+        Child 3
+      </div>
+    </div>
+  )
+}
+
 export default App
