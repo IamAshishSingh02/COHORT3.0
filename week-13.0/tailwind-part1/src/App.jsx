dd47367f9532b840c066712827b9a1ee666e195e
Ashish Singh <psssasds212004@gmail.com>
2025-11-17 18:12:42 +0000

    commenting previous work and implemented a small practice assignment of tailwind css

--- week-13.0/tailwind-part1/src/App.jsx
+++ week-13.0/tailwind-part1/src/App.jsx
@@ -24,8 +24,20 @@ const App = () => {
     //   </div>
     // </div>
 
-    <div className="xl:bg-yellow-300 md:bg-green-300 sm:bg-blue-300 bg-red-300">
-      Hi there
+    // <div className="xl:bg-yellow-300 md:bg-green-300 sm:bg-blue-300 bg-red-300">
+    //   Hi there
+    // </div>
+
+    <div className="sm:grid grid-cols-12 ">
+      <div className="bg-blue-300 col-span-4">
+        Child 1
+      </div>
+      <div className="bg-red-300 col-span-5">
+        Child 2
+      </div>
+      <div className="bg-green-300 col-span-3">
+        Child 3
+      </div>
     </div>
   )
 }
