fbd8e599f7c20e61740e955b1e5f8dd63f4df23f
Ashish Singh <psssasds212004@gmail.com>
2025-11-17 17:30:48 +0000

    learned about grid in tailwind and commenting previous work

--- week-13.0/tailwind-part1/src/App.jsx
+++ week-13.0/tailwind-part1/src/App.jsx
@@ -1,13 +1,25 @@
 const App = () => {
   return(
-    <div className="flex justify-around ">
-      <div className="bg-red-300">
+    // <div className="flex justify-around">
+    //   <div className="bg-red-300">
+    //     Child 1
+    //   </div>
+    //   <div className="bg-blue-300">
+    //     Child 2
+    //   </div>
+    //   <div className="bg-green-300">
+    //     Child 3
+    //   </div>
+    // </div>
+
+    <div className="grid grid-cols-12">
+      <div className="bg-red-300 col-span-4">
         Child 1
       </div>
-      <div className="bg-blue-300">
+      <div className="bg-blue-300 col-span-5">
         Child 2
       </div>
-      <div className="bg-green-300">
+      <div className="bg-green-300 col-span-3">
         Child 3
       </div>
     </div>
