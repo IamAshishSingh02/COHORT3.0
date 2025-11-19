a75ebb9acfe857d16061ac7569ed548898b28a54
Ashish Singh <psssasds212004@gmail.com>
2025-11-19 12:56:41 +0000

    created generic otp component

--- week-13.0/tailwind-part1/src/components/Otp.jsx
+++ week-13.0/tailwind-part1/src/components/Otp.jsx
@@ -1,29 +1,66 @@
-import { useRef } from "react"
+import {Button} from "./button";
+import { useRef, useState, useEffect } from "react";
 
-export const Otp = () => {
-  const ref1 = useRef()
-  const ref2 = useRef()
-  const ref3 = useRef()
-  const ref4 = useRef()
-  const ref5 = useRef()
-  const ref6 = useRef()
+export const Otp = ({number}) => {
+    const ref = useRef(Array(number).fill(0));
+    const [values, setValues] = useState(Array(number).fill(""));
+    const [disabled, setDisabled] = useState(true);
 
-  return(
-    <div className="flex justify-center">
-      <SubOtpBox ref = {ref1} onDone = {() => {ref2.current.focus()}} />
-      <SubOtpBox ref = {ref2} onDone = {() => {ref3.current.focus()}} />
-      <SubOtpBox ref = {ref3} onDone = {() => {ref4.current.focus()}} />
-      <SubOtpBox ref = {ref4} onDone = {() => {ref5.current.focus()}} />
-      <SubOtpBox ref = {ref5} onDone = {() => {ref6.current.focus()}} />
-      <SubOtpBox ref = {ref6} />
-    </div>
-  )
+    useEffect(() => {
+        setDisabled(values.some(v => v === ""));
+    }, [values]);
+
+    return (
+        <div className="flex flex-col justify-center items-center gap-4">
+            <div className="flex flex-row">
+                {values.map((v, index) => (
+                    <SubOtpBox
+                        key={index}
+                        reference={(e) => (ref.current[index] = e)}
+                        onChange={(digit) => {
+                            const temp = [...values];
+                            temp[index] = digit;
+                            setValues(temp);
+                        }}
+                        onDone={() => index + 1 < number && ref.current[index + 1].focus()}
+                        goBack={() => index > 0 && ref.current[index - 1].focus()}
+                    />
+                ))}
+            </div>
+
+            <Button disabled={disabled}>Sign up</Button>
+        </div>
+    );
 }
 
-const SubOtpBox = ({ref, onDone}) => {
-  return(
-    <div>
-      <input ref = {ref} onChange={(e) => {onDone()}} type="text" className="m-1 w-[42px] h-[50px] rounded-xl bg-blue-500 outline-none text-center text-white" />
-    </div>
-  )
-}
+
+const SubOtpBox = ({ reference, onDone, goBack, onChange }) => {
+    const [inputBoxVal, setInputBoxVal] = useState("");
+
+    return (
+        <input
+            value={inputBoxVal}
+            ref={reference}
+            type="text"
+            maxLength={1}
+            className="m-1 w-[42px] h-[50px] rounded-xl bg-blue-500 outline-none px-4 text-white text-center"
+            onKeyDown={(e) => {
+                if (e.key === "Backspace" && inputBoxVal === "") {
+                    goBack();
+                }
+            }}
+            onChange={(e) => {
+                const val = e.target.value;
+
+                if (/^[0-9]$/.test(val)) {
+                    setInputBoxVal(val);
+                    onChange(val);
+                    onDone();       // MOVE FORWARD
+                } else {
+                    setInputBoxVal("");
+                    onChange("");
+                }
+            }}
+        />
+    );
+};
