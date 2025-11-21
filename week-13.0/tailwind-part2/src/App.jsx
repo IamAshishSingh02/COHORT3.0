3c7456ac1e9989055e0b00cf0fd07f293880b169
Ashish Singh <psssasds212004@gmail.com>
2025-11-21 21:44:33 +0000

    trying mannual dark mode toggling in react using tailwindcss

--- week-13.0/tailwind-part2/src/App.jsx
+++ week-13.0/tailwind-part2/src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      
+        <div className="h-screen bg-white dark:bg-black text-black dark:text-white">
+          <button onClick={() => {
+            document.querySelector("html").classList.toggle("dark")
+          }}>
+            Dark Mode
+          </button>
+        </div>
     </>
   )
 }
