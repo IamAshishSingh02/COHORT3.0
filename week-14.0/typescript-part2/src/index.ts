a51a3e4185ad2592b77c9262aaad2c58f7c41cfd
Ashish Singh <psssasds212004@gmail.com>
2025-11-27 21:25:33 +0000

    renamed the folder name from week-14 to week-14.0

---
+++ week-14.0/typescript-part2/src/index.ts
@@ -0,0 +1,50 @@
+// // created a interface with optional field address - may or may not present - use q mark
+// interface Address{
+//     city: string,
+//     country: string,
+//     pincode: number
+//   }
+// interface User{
+//   name: string,
+//   age: number,
+//   address?: Address
+// }
+// interface Office{
+//   address: Address
+// }
+// // Address field present
+// const user1 = {
+//   name: "Ashish",
+//   age: 21,
+//   address: {
+//     city: "Fatehgarh",
+//     country: "India",
+//     pincode: 209601
+//   }
+// }
+// // Address field not present
+// const user2 = {
+//   name: "As",
+//   age: 21
+// }
+
+// Creating a class that implements the interface
+interface User{
+  name: string,
+  age: number,
+  isLegal(): boolean // isLegal: () => boolean
+}
+class Manager implements User{
+  constructor(public name: string, public age:number){
+    this.name = name
+    this.age = age
+  }
+
+  isLegal(): boolean {
+    return this.age > 18
+  }
+}
+const user = new Manager("Ashish", 21) // Object of the class
+console.log(user.name);
+console.log(user.age);
+console.log(user.isLegal());
