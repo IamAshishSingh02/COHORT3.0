d911896f62cdf87773dc442615518009ba337cd7
Ashish Singh <psssasds212004@gmail.com>
2025-11-28 20:46:32 +0000

    Created a class that extends the abstract class

--- week-14.0/typescript-part2/src/index.ts
+++ week-14.0/typescript-part2/src/index.ts
@@ -28,23 +28,48 @@
 //   age: 21
 // }
 
-// Creating a class that implements the interface
-interface User{
-  name: string,
-  age: number,
-  isLegal(): boolean // isLegal: () => boolean
-}
-class Manager implements User{
-  constructor(public name: string, public age:number){
+// // Creating a class that implements the interface
+// interface User{
+//   name: string,
+//   age: number,
+//   isLegal(): boolean // isLegal: () => boolean
+// }
+// class Manager implements User{
+//   constructor(public name: string, public age:number){
+//     this.name = name
+//     this.age = age
+//   }
+
+//   isLegal(): boolean {
+//     return this.age > 18
+//   }
+// }
+// const user = new Manager("Ashish", 21) // Object of the class
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.isLegal());
+
+// Creating a class that extends the abstract class
+abstract class User{
+  constructor(public name: string){
     this.name = name
-    this.age = age
   }
 
-  isLegal(): boolean {
-    return this.age > 18
+  // Abstract method
+  abstract greet(): string
+
+  // Abstract classes can have default implementations which interfaces does not have
+  hello(){
+    console.log("Hi There");
   }
 }
-const user = new Manager("Ashish", 21) // Object of the class
-console.log(user.name);
-console.log(user.age);
-console.log(user.isLegal());
+class Employee extends User{
+  constructor(public name: string){
+    super(name)
+    this.name = name
+  }
+
+  greet(){
+    return("Hi " + this.name);
+  }
+}
