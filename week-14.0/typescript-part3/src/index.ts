c6520b8173114904efc8378c6e3deb5ce445823c
Ashish Singh <psssasds212004@gmail.com>
2025-12-01 16:16:06 +0000

    learned about pick, partial, readonly in ts

---
+++ week-14.0/typescript-part3/src/index.ts
@@ -0,0 +1,24 @@
+interface User{
+  id: string,
+  name: string,
+  age: number,
+  email: string,
+  password: string
+}
+// Pick lets you select set of properties from an existing type or interface
+type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
+// Partial lets you make all properties optional of an existing type or interface
+type UpdatePropsOptional = Partial<UpdateProps>
+// Readonly make the internal properties as const as well 
+type UserType = {
+  // one way of making read only
+  // readonly name: string,
+  // readonly age: number
+  name: string,
+  age: number
+}
+const user: Readonly<UserType> = {
+  name: "Ashish",
+  age: 21
+}
+// user.age = 22 // won't work because its read only
