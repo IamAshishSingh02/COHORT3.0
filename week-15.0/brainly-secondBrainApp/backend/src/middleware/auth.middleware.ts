11dc203392ec23032641ba841eeb103e4430378b
Ashish Singh <psssasds212004@gmail.com>
2026-01-17 14:01:05 +0000

    feat(auth): add JWT authentication middleware

---
+++ week-15.0/brainly-secondBrainApp/backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from "express"
+import jwt from "jsonwebtoken";
+
+export const authMiddleware: RequestHandler = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")){
+    return res.status(401).json({
+      message: "Unauthorized: Token missing"
+    })
+  }
+
+  const token = authHeader.split(" ")[1]
+
+  try{
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as { userId: string }
+
+    res.locals.userId = decoded.userId
+    next()
+  } 
+  catch(error){
+    return res.status(401).json({
+      message: "Unauthorized: Invalid token"
+    })
+  }
+}
