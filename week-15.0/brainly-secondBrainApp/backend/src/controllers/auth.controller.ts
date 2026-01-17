ea7701cc0a712a08cde40ffe0bca9861a6275671
Ashish Singh <psssasds212004@gmail.com>
2026-01-17 14:00:01 +0000

    feat(auth): implement signup and signin controllers

---
+++ week-15.0/brainly-secondBrainApp/backend/src/controllers/auth.controller.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import User from "../models/user.model"
+
+export const signup = async (req: Request, res: Response) => {
+  try{
+    const {name, email, password} = req.body
+
+    const existingUser = await User.findOne({email})
+    if(existingUser){
+      return res.status(409).json({
+        message: "User already exists"
+      })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    })
+
+    return res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    })
+  }
+  catch(error){
+    return res.status(500).json({
+      message: "Signup failed"
+    })
+  }
+}
+
+export const signin = async (req: Request, res: Response) => {
+  try{
+    const {email, password} = req.body 
+
+    const user = await User.findOne({email})
+    if(!user){
+      return res.status(401).json({
+        message: "Invalid Credentials"
+      })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    const token = jwt.sign(
+      {userId: user._id}, 
+      process.env.JWT_SECRET as string, 
+      {expiresIn: "7d"}
+    )
+
+    return res.status(200).json({
+      message: "SignIn Successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    })
+  }
+  catch(error){
+    return res.status(500).json({
+      message: "SingIn failed"
+    })
+  }
+}
