47a6126dbe89cb4adbd937cd82e2193ca9eb5275
Ashish Singh <psssasds212004@gmail.com>
2026-01-28 18:42:58 +0000

    learned about basic web socket server

--- week-16.0/websockets/src/index.ts
+++ week-16.0/websockets/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hi There");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port: 8080})
+
+wss.on('connection', (socket) => {
+  console.log('User Connected')
+
+  socket.on('message', (e) => {
+    if(e.toString() ==="ping"){
+      socket.send("pong")
+    }
+  })
+})
