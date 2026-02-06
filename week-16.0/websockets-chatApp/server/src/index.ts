21894d4b0abfd8cdda3e0d4c6048cba977ec1b54
IamAshishSingh02 <psssasds212004@gmail.com>
2026-02-06 11:19:43 +0000

    move the whole wss into server folder

---
+++ week-16.0/websockets-chatApp/server/src/index.ts
@@ -0,0 +1,58 @@
+import { WebSocketServer, WebSocket } from 'ws'
+
+const wss = new WebSocketServer({ port: 8080 })
+
+interface User {
+  socket: WebSocket,
+  room: string
+}
+
+interface JoinMessage {
+  type: 'join'
+  payload: {
+    roomId: string,
+  }
+}
+
+interface ChatMessage {
+  type: 'chat'
+  payload: {
+    message: string
+  }
+}
+
+type IncomingMessage = JoinMessage | ChatMessage
+
+let allSockets: User[] = []
+
+wss.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    const parsedMessage: IncomingMessage = JSON.parse(message.toString())
+
+    if(parsedMessage.type === 'join'){
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId
+      })
+    }
+
+    else if(parsedMessage.type === 'chat'){
+      const sender = allSockets.find(u => u.socket === socket)
+
+      if(!sender) return
+
+      const message = parsedMessage.payload.message
+      if(!message) return
+
+      allSockets.forEach(u => {
+        if(u.room ===sender.room){
+          u.socket.send(message)
+        }
+      });
+    }
+  })
+
+  socket.on("close", () => {
+    allSockets = allSockets.filter(u => u.socket !== socket)
+  })
+})
